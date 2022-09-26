ee3615cbc163a5d2bfe675ee76c73a53bbd7d6cc
yujie <yujieyujiedayujie@gmail.com>
2022-09-26 11:17:31 +0800

    format

--- chimpanzeeTest.py
+++ chimpanzeeTest.py
@@ -15,15 +15,19 @@ while True:
     rows = driver.find_elements(By.XPATH, "//div[@class='css-gmuwbf']/div/div")
     b = []
     for y in range(len(rows)):
-        squares = driver.find_elements(By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div".format(y+1))
+        squares = driver.find_elements(
+            By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div".format(y+1))
         for x in range(len(squares)):
-            a = driver.find_element(By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div[{}]".format(y+1, x+1)).get_attribute("data-cellnumber")
+            a = driver.find_element(By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div[{}]".format(
+                y+1, x+1)).get_attribute("data-cellnumber")
             if a:
                 b.append([int(a), y, x])
     b.sort()
     for i in b:
-        driver.find_element(By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div[{}]".format(i[1]+1, i[2]+1)).click()
+        driver.find_element(
+            By.XPATH, "//div[@class='css-gmuwbf']/div/div[{}]/div[{}]".format(i[1]+1, i[2]+1)).click()
     time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, "button.css-de05nr.e19owgy710").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "button.css-de05nr.e19owgy710").click()
 
 time.sleep(20)
